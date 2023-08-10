c52c19a089bcb6b8a85166887595e5b282c31d9f
ytgui <ytgui95@outlook.com>
2023-08-10 14:44:09 +0800

    refine mha test by using sparse top-k indices

--- test/kernel/test_mha_1.py
+++ test/kernel/test_mha_1.py
@@ -44,36 +44,43 @@ def main():
         [16, 32, 48, 64]
     )
     n_heads = random.randint(1, 8)
-    seq_length = random.randint(1, 256)
+    seq_length = 16 * random.randint(1, 16)
     cuda_device = 'cuda'
 
     #
-    q = torch.ones(
+    q = torch.randn(
         [seq_length, d_head],
         requires_grad=True,
         device=cuda_device
     )
-    k = torch.ones(
+    k = torch.randn(
         [seq_length, d_head],
         requires_grad=True,
         device=cuda_device
+    )
+    attn = torch.einsum(
+        'ie, je -> ij', q, k
     )
 
-    # mask
-    prob = torch.rand(
-        [seq_length, seq_length],
-        device=cuda_device
+    # sparse
+    top_k = attn.size(-1) // 4
+    top_output = torch.topk(
+        attn, k=top_k, dim=-1,
+        largest=True, sorted=False
     )
-    mask = torch.where(
-        prob < 0.15, True, False
+    top_values, top_indices = top_output
+    csr_indices = torch.flatten(top_indices)
+    fixed_indptr = torch.arange(
+        0, top_k * (attn.size(-2) + 1),
+        step=top_k, device=cuda_device
     )
-    sparse = mask.to_sparse_csr()
+    sparse_attn = torch.sparse_csr_tensor(
+        crow_indices=fixed_indptr, col_indices=top_indices,
+        values=top_values, size=attn.size()
+    )
 
     # built-in
-    y_1 = torch.matmul(q, k.T)
-    y_1 = torch.masked_select(
-        y_1, mask=mask
-    )
+    y_1 = torch.flatten(top_values)
     torch.sum(y_1).backward()
     grad_q_1 = q.grad.detach().clone()
     grad_k_1 = k.grad.detach().clone()
@@ -81,10 +88,8 @@ def main():
     # custom kernel
     q.grad = None
     k.grad = None
-    indptr_2 = sparse.crow_indices()
-    indices_2 = sparse.col_indices()
     y_2 = sparse_mha(
-        indptr_2, indices_2, q, k
+        fixed_indptr, csr_indices, q, k
     )
     torch.sum(y_2).backward()
     grad_q_2 = q.grad.detach().clone()

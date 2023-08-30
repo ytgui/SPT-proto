aca2bb3e2deb37a7c81943a84aa2749456a503cc
ytgui <ytgui95@outlook.com>
2023-08-30 17:49:51 +0800

    benchmark cdist kernel

--- test/kernel/test_cdist.py
+++ test/kernel/test_cdist.py
@@ -1,5 +1,7 @@
+import time
 import torch
 import random
+from torch import profiler
 from naive_gpt import kernels
 
 
@@ -43,8 +45,58 @@ def test_cdist():
     print('[PASS] test_cdist()')
 
 
+def bench_cdist():
+    d_code = 4
+    n_queries = 16384
+    n_codewords = 64
+    n_subspaces = 16
+    cuda_device = 'cuda'
+
+    #
+    query = torch.randn(
+        [n_subspaces, n_queries, d_code],
+        device=cuda_device, requires_grad=True
+    )
+    table = torch.randn(
+        [n_subspaces, n_codewords, d_code],
+        device=cuda_device, requires_grad=True
+    )
+
+    # cdist
+    time.sleep(2.0)
+    with profiler.profile(
+        activities=[profiler.ProfilerActivity.CUDA],
+        profile_memory=True, with_flops=True
+    ) as prof:
+        for _ in range(20):
+            y_1 = torch.cdist(query, table, p=1.0)
+    print(
+        prof.key_averages().table(
+            sort_by='cuda_time_total', row_limit=5
+        )
+    )
+
+    # kernel
+    time.sleep(2.0)
+    with profiler.profile(
+        activities=[profiler.ProfilerActivity.CUDA],
+        profile_memory=True, with_flops=True
+    ) as prof:
+        for _ in range(20):
+            y_2 = kernels.pq_cdist(query, table)
+    print(
+        prof.key_averages().table(
+            sort_by='cuda_time_total', row_limit=5
+        )
+    )
+
+    #
+    print('[PASS] bench_cdist()')
+
+
 def main():
     test_cdist()
+    bench_cdist()
 
 
 if __name__ == '__main__':

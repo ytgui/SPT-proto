b2a92fd3a0c99fe394eeb8c39cd69b7e39b195ec
ytgui <ytgui95@outlook.com>
2023-09-11 13:05:10 +0800

    bugfix and benchmark for lookup kernel

--- extension/lookup.cu
+++ extension/lookup.cu
@@ -1,12 +1,11 @@
 #include "common.h"
 
 #define TSZ 4
-#define BSZ 16
-#define MAX_NNZ 256
+#define BSZ 64
 using vector_t = int4;
 
 // clang-format off
-template <unsigned NSZ>
+template <unsigned NSP, unsigned NNZ>
 __global__ void lookup_forward_kernel(
     index_t batch_size, index_t seq_length, index_t nonzeros,
     const index_t *query, const index_t *store, index_t *output
@@ -17,27 +16,27 @@ __global__ void lookup_forward_kernel(
     index_t gy = blockIdx.y * blockDim.y + ty;
 
     // cache
-    __shared__ index_t cache_query[BSZ][NSZ];
-    for (index_t offset_k = 0; offset_k < NSZ; offset_k += TSZ) {
-        *(vector_t *)&cache_query[ty][offset_k] = __ldg(
+    index_t cache_query[NSP];
+    for (index_t k = 0; k < NSP; k += TSZ) {
+        *(vector_t *)&cache_query[k] = __ldg(
             (const vector_t *)&query[
-                gz * seq_length * NSZ + gy * NSZ + offset_k
+                gz * seq_length * NSP + gy * NSP + k
             ]
         );
     }
 
     // result
-    index_t size_indices[NSZ / 2] = {};
-    index_t cache_indices[NSZ / 2][MAX_NNZ];
+    index_t cache_sizes[NSP / 2] = {};
+    index_t cache_indices[NSP / 2][NNZ];
 
     // window
     for (index_t offset_x = 0; offset_x < seq_length; offset_x += BSZ) {
         // cache
-        __shared__ index_t cache_store[BSZ][NSZ];
-        for (index_t offset_k = 0; offset_k < NSZ; offset_k += TSZ) {
-            *(vector_t *)&cache_store[ty][offset_k] = __ldg(
+        __shared__ index_t cache_store[BSZ][NSP];
+        for (index_t k = 0; k < NSP; k += TSZ) {
+            *(vector_t *)&cache_store[ty][k] = __ldg(
                 (const vector_t *)&store[
-                    gz * seq_length * NSZ + (offset_x + ty) * NSZ + offset_k
+                    gz * seq_length * NSP + (offset_x + ty) * NSP + k
                 ]
             );
         }
@@ -46,26 +45,33 @@ __global__ void lookup_forward_kernel(
         // product
         for (index_t tx = 0; tx < BSZ; tx += 1) {
             index_t count = 0;
-            for (index_t k = 0; k < NSZ; k += 1) {
-                count += (cache_query[ty][k] == cache_store[tx][k]);   
+            for (index_t k = 0; k < NSP; k += 1) {
+                count += (cache_query[k] == cache_store[tx][k]);   
             }
-            index_t cursor = size_indices[count / 2];
-            cache_indices[count / 2][cursor] = offset_x + tx;
-            size_indices[count / 2] += 1;
+            index_t slot = min(count / 2, NSP / 2 - 1);
+            index_t cursor = min(cache_sizes[slot], NNZ - 1);
+            cache_indices[slot][cursor] = offset_x + tx;
+            cache_sizes[slot] += 1;
         }
         __syncthreads();
     }
 
     // store
-    index_t cursor_i = NSZ / 2 - 1, cursor_j = 0;
-    for (index_t gx = 0; gx < nonzeros; gx += 1) {
-        if (cursor_j >= size_indices[cursor_i]) {
-            cursor_i -= 1; cursor_j = 0;
+    index_t slot = NSP / 2 - 1, cursor = 0;
+    for (index_t gx = 0; gx < nonzeros; gx += TSZ) {
+        index_t buffer[TSZ];
+        for (index_t t = 0; t < TSZ; t += 1) {
+            buffer[t] = cache_indices[slot][cursor];
+            // move slot and cursor
+            index_t limit = cache_sizes[slot];
+            slot -= (cursor + 1) / limit;
+            cursor = (cursor + 1) % limit;
         }
-        output[
-            gz * seq_length * nonzeros + gy * nonzeros + gx
-        ] = cache_indices[cursor_i][cursor_j];
-        cursor_j += 1;
+        __stcs(
+            (vector_t *)&output[
+                gz * seq_length * nonzeros + gy * nonzeros + gx
+            ], *(const vector_t *)buffer
+        );
     }
 }
 // clang-format on
@@ -91,7 +97,6 @@ torch::Tensor lookup_forward_cuda(
     TORCH_CHECK(seq_length % sparsity == 0);
     index_t nonzeros = seq_length / sparsity;
     TORCH_CHECK(nonzeros % BSZ == 0);
-    TORCH_CHECK(nonzeros <= MAX_NNZ);
     auto output = torch::empty(
         {batch_size, seq_length, nonzeros}, query.options()
     );
@@ -100,15 +105,19 @@ torch::Tensor lookup_forward_cuda(
     dim3 threads(1, BSZ);
     dim3 blocks(1, seq_length / BSZ, batch_size);
     if (n_subspaces == 8) {
-        lookup_forward_kernel<8><<<blocks, threads>>>(
-            batch_size, seq_length, nonzeros, query.data_ptr<index_t>(),
-            store.data_ptr<index_t>(), output.data_ptr<index_t>()
-        );
-    } else if (n_subspaces == 16) {
-        lookup_forward_kernel<16><<<blocks, threads>>>(
-            batch_size, seq_length, nonzeros, query.data_ptr<index_t>(),
-            store.data_ptr<index_t>(), output.data_ptr<index_t>()
-        );
+        if (seq_length / sparsity <= 64) {
+            lookup_forward_kernel<8, 64><<<blocks, threads>>>(
+                batch_size, seq_length, nonzeros, query.data_ptr<index_t>(),
+                store.data_ptr<index_t>(), output.data_ptr<index_t>()
+            );
+        } else if (seq_length / sparsity <= 256) {
+            lookup_forward_kernel<8, 256><<<blocks, threads>>>(
+                batch_size, seq_length, nonzeros, query.data_ptr<index_t>(),
+                store.data_ptr<index_t>(), output.data_ptr<index_t>()
+            );
+        } else {
+            TORCH_CHECK(false && "seq_length / sparsity not supported");
+        }
     } else {
         TORCH_CHECK(false && "n_subspaces not supported");
     }
--- test/kernel/test_lookup.py
+++ test/kernel/test_lookup.py
@@ -1,4 +1,6 @@
+import time
 import torch
+from torch import profiler
 from torch import autograd
 from naive_gpt import ext
 
@@ -9,13 +11,11 @@ class Lookup(autograd.Function):
                 config: torch.Tensor,
                 query: torch.Tensor,
                 store: torch.Tensor):
-        # ctx.save_for_backward(query, store)
         return ext.lookup_forward_cuda(config, query, store)
 
     @staticmethod
     def backward(ctx,
                  grad_output: torch.Tensor):
-        # query, table = ctx.saved_tensors
         raise NotImplementedError
 
 
@@ -26,11 +26,10 @@ def lookup(query: torch.Tensor,
     return Lookup.apply(config, query, store)
 
 
-def test_lookup():
-    n_subspaces = 8
-    n_codewords = 4
-    seq_length = 256
-    batch_size = 16
+def get_input(n_subspaces: int,
+              n_codewords: int,
+              seq_length: int,
+              batch_size: int):
     cuda_device = 'cuda'
 
     #
@@ -46,6 +45,14 @@ def test_lookup():
         ],
         dtype=torch.int32, device=cuda_device
     )
+    return query, store
+
+
+def test_lookup():
+    query, store = get_input(
+        n_subspaces=8, n_codewords=4,
+        seq_length=512, batch_size=64
+    )
 
     # builtin
     cmp = torch.eq(
@@ -54,7 +61,7 @@ def test_lookup():
     )
     reduced = torch.sum(cmp, dim=-1)
     topk_output = torch.topk(
-        reduced, k=seq_length // 8, dim=-1
+        reduced, k=reduced.size(-1) // 8, dim=-1
     )
     topk_indices = topk_output.indices
     y_1 = torch.flatten(topk_indices, end_dim=-2)
@@ -64,22 +71,70 @@ def test_lookup():
     y_2 = torch.flatten(lookup_indices, end_dim=-2)
 
     # check
-    accuracy = []
+    recall = []
     assert y_1.size() == y_2.size()
     for lhs, rhs in zip(y_1.tolist(), y_2.tolist()):
-        union = set(lhs).union(rhs)
         count = set(lhs).intersection(rhs)
-        accuracy.append(len(count) / len(union))
-    accuracy = sum(accuracy) / len(accuracy)
-    print('accuracy:', accuracy)
-    assert accuracy > 0.8
+        recall.append(len(count) / len(lhs))
+    recall = sum(recall) / len(recall)
+    print('recall:', recall)
+    assert recall > 0.8
 
     #
     print('[PASS] test_lookup()')
 
 
+def bench_lookup():
+    batch_size = 64
+    n_subspaces = 8
+    query, store = get_input(
+        n_subspaces=n_subspaces, n_codewords=4,
+        seq_length=512, batch_size=batch_size
+    )
+    x_1 = torch.randn(
+        [batch_size, 512, n_subspaces * 8],
+        device=query.device
+    )
+    x_2 = torch.randn(
+        [batch_size, n_subspaces * 8, 512],
+        device=query.device
+    )
+
+    # matmul
+    time.sleep(2.0)
+    with profiler.profile(
+        activities=[profiler.ProfilerActivity.CUDA],
+        profile_memory=True, with_flops=True
+    ) as prof:
+        for _ in range(200):
+            torch.matmul(x_1, x_2)
+    print(
+        prof.key_averages().table(
+            sort_by='cuda_time_total', row_limit=5
+        )
+    )
+
+    # kernel
+    time.sleep(2.0)
+    with profiler.profile(
+        activities=[profiler.ProfilerActivity.CUDA],
+        profile_memory=True, with_flops=True
+    ) as prof:
+        for _ in range(200):
+            lookup(query, store, sparsity=8)
+    print(
+        prof.key_averages().table(
+            sort_by='cuda_time_total', row_limit=5
+        )
+    )
+
+    #
+    print('[PASS] bench_lookup()')
+
+
 def main():
     test_lookup()
+    bench_lookup()
 
 
 if __name__ == '__main__':

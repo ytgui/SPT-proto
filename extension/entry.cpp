0fb0287219f051dc6dd1ced1b4db0baffbb5e5ec
ytgui <ytgui95@outlook.com>
2023-09-12 11:19:58 +0800

    add a cuda matmul baseline as template for other kernels

--- extension/entry.cpp
+++ extension/entry.cpp
@@ -4,6 +4,10 @@
 #include <torch/extension.h>
 // clang-format on
 
+torch::Tensor matmul_cuda(
+    const torch::Tensor &left, const torch::Tensor &right
+);
+
 torch::Tensor cdist_forward_cuda(
     const torch::Tensor &query, const torch::Tensor &table
 );
@@ -54,6 +58,8 @@ std::vector<torch::Tensor> blkmv_backward_cuda(
 );
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    // gemm
+    m.def("matmul_cuda", &matmul_cuda, "matmul");
     // cdist
     m.def("cdist_forward_cuda", &cdist_forward_cuda, "cdist forward");
     m.def("cdist_backward_cuda", &cdist_backward_cuda, "cdist backward");

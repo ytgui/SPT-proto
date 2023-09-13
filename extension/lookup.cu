fd23a0e12c4299e7fee0579b9b05931e634ec48a
ytgui <ytgui95@outlook.com>
2023-09-13 11:24:38 +0800

    lookup kernel now achieves 1.7x speedup

--- extension/lookup.cu
+++ extension/lookup.cu
@@ -1,75 +1,78 @@
 #include "common.h"
 
-#define TSZ 4
-#define BSZ 16
+#define TILE_SIZE 4
+#define BLOCK_SIZE 32
+#define WORKER_SIZE 2
 
 using vector_t = int4;
 
 // clang-format off
-template <unsigned NSP, unsigned MAXNZ>
+template <unsigned N_SPACES>
 __global__ void lookup_forward_kernel(
     index_t batch_size, index_t seq_length, index_t nonzeros,
-    const index_t *query, const index_t *store, index_t *output
+    const index_t *left, const index_t *right, index_t *output
 ) {
     // index
     index_t ty = threadIdx.y;
+    index_t tx = threadIdx.x;
     index_t gz = blockIdx.z * blockDim.z;
     index_t gy = blockIdx.y * blockDim.y + ty;
 
-    // cache
-    index_t cache_query[NSP];
-    for (int16_t k = 0; k < NSP; k += 1) {
-        cache_query[k] = query[
-            gz * seq_length * NSP + gy * NSP + k
-        ];
+    // load lhs
+    __shared__ index_t cache_lhs[BLOCK_SIZE][N_SPACES];
+    for (index_t k = tx * TILE_SIZE; k < N_SPACES; k += WORKER_SIZE * TILE_SIZE) {
+        *(vector_t *)&cache_lhs[ty][k] = __ldg(
+            (const vector_t *)&left[
+                gz * seq_length * N_SPACES + gy * N_SPACES + k
+            ]
+        );
     }
-
-    // result
-    #define NSLOTS 3
-    int16_t cache_sizes[NSLOTS] = {};
-    int16_t cache_indices[NSLOTS][MAXNZ];
 
     // window
-    for (int16_t offset_x = 0; offset_x < seq_length; offset_x += BSZ) {
-        // cache
-        __shared__ index_t cache_store[BSZ][NSP];
-        for (int16_t k = 0; k < NSP; k += 1) {
-            cache_store[ty][k] = store[
-                gz * seq_length * NSP + (offset_x + ty) * NSP + k
-            ];
+    index_t offset_output = 0;
+    for (index_t offset_x = 0; offset_x < seq_length; offset_x += BLOCK_SIZE) {
+        // load rhs
+        __shared__ index_t cache_rhs[BLOCK_SIZE][N_SPACES];
+        for (index_t k = tx * TILE_SIZE; k < N_SPACES; k += WORKER_SIZE * TILE_SIZE) {
+            *(vector_t *)&cache_rhs[ty][k] = __ldg(
+                (const vector_t *)&right[
+                    gz * seq_length * N_SPACES + (offset_x + ty) * N_SPACES + k
+                ]
+            );
         }
         __syncthreads();
 
-        // product
-        for (int16_t tx = 0; tx < BSZ; tx += 1) {
-            int16_t count = 0;
-            for (int16_t k = 0; k < NSP; k += 1) {
-                count += (cache_query[k] == cache_store[tx][k]);   
+        // lookup
+        index_t cursors[4] = {tx, tx, tx, tx};
+        __shared__ uint16_t indices[BLOCK_SIZE][4][BLOCK_SIZE];
+        for (index_t local_x = tx; local_x < BLOCK_SIZE; local_x += WORKER_SIZE) {
+            index_t count = 0;
+            for (index_t k = 0; k < N_SPACES; k += 1) {
+                count += (
+                    cache_lhs[ty][k] == cache_rhs[local_x][k]
+                );
             }
-            count = min(7, count) / 2;
-            int16_t slot = 32 - __clz(count);
-            int16_t cursor = min(cache_sizes[slot], MAXNZ - 1);
-            cache_indices[slot][cursor] = offset_x + tx;
-            cache_sizes[slot] += 1;
+            count = min(3, count / 2);
+            index_t slot = 32 - __clz(count);
+            index_t cursor = cursors[slot];
+            indices[ty][slot][cursor] = offset_x + local_x;
+            cursors[slot] = min(cursor + WORKER_SIZE, BLOCK_SIZE - tx - 1);
         }
         __syncthreads();
-    }
 
-    // store
-    int16_t slot = NSLOTS - 1, cursor = 0;
-    for (int16_t gx = 0; gx < nonzeros; gx += TSZ) {
-        index_t buffer[TSZ];
-        for (index_t t = 0; t < TSZ; t += 1) {
-            buffer[t] = cache_indices[slot][cursor];
-            bool cond = (cursor == cache_sizes[slot] - 1);
-            cursor = cond ? 0 : (cursor + 1);
-            slot = cond ? (slot - 1) : slot;
+        // store
+        index_t slot = 4 - 1, cursor = tx;
+        index_t n_times = seq_length / BLOCK_SIZE;
+        index_t offset_b = gz * seq_length * nonzeros;
+        for (index_t local_x = tx; local_x < nonzeros / n_times; local_x += WORKER_SIZE) {
+            while (cursor >= cursors[slot]) {
+                slot = slot - 1; cursor = tx;
+            }
+            index_t indice = indices[ty][slot][cursor];
+            output[offset_b + gy * nonzeros + offset_output + local_x] = indice;
+            cursor = cursor + WORKER_SIZE;
         }
-        __stcs(
-            (vector_t *)&output[
-                gz * seq_length * nonzeros + gy * nonzeros + gx
-            ], *(const vector_t *)buffer
-        );
+        offset_output += nonzeros / n_times;
     }
 }
 // clang-format on
@@ -90,37 +93,22 @@ torch::Tensor lookup_forward_cuda(
     index_t batch_size = query.size(0);
     index_t seq_length = query.size(1);
     index_t n_subspaces = query.size(-1);
-    TORCH_CHECK(seq_length % BSZ == 0);
-    TORCH_CHECK(n_subspaces % TSZ == 0);
+    TORCH_CHECK(seq_length % BLOCK_SIZE == 0);
     TORCH_CHECK(seq_length % sparsity == 0);
     index_t nonzeros = seq_length / sparsity;
-    TORCH_CHECK(nonzeros % BSZ == 0);
+    TORCH_CHECK(nonzeros % BLOCK_SIZE == 0);
     auto output = torch::empty(
         {batch_size, seq_length, nonzeros}, query.options()
     );
 
     // dispatch
-    dim3 threads(1, BSZ);
-    dim3 blocks(1, seq_length / BSZ, batch_size);
+    dim3 blocks(1, seq_length / BLOCK_SIZE, batch_size);
     if (n_subspaces == 8) {
-        if (nonzeros <= 64) {
-            lookup_forward_kernel<8, 64><<<blocks, threads>>>(
-                batch_size, seq_length, nonzeros, query.data_ptr<index_t>(),
-                store.data_ptr<index_t>(), output.data_ptr<index_t>()
-            );
-        } else if (nonzeros <= 128) {
-            lookup_forward_kernel<8, 128><<<blocks, threads>>>(
-                batch_size, seq_length, nonzeros, query.data_ptr<index_t>(),
-                store.data_ptr<index_t>(), output.data_ptr<index_t>()
-            );
-        } else if (nonzeros <= 256) {
-            lookup_forward_kernel<8, 256><<<blocks, threads>>>(
-                batch_size, seq_length, nonzeros, query.data_ptr<index_t>(),
-                store.data_ptr<index_t>(), output.data_ptr<index_t>()
-            );
-        } else {
-            TORCH_CHECK(false && "seq_length / sparsity not supported");
-        }
+        dim3 threads(WORKER_SIZE, BLOCK_SIZE);
+        lookup_forward_kernel<8><<<blocks, threads>>>(
+            batch_size, seq_length, nonzeros, query.data_ptr<index_t>(),
+            store.data_ptr<index_t>(), output.data_ptr<index_t>()
+        );
     } else {
         TORCH_CHECK(false && "n_subspaces not supported");
     }

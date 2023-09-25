95b96769938510805a8257a38bd158189760000c
ytgui <ytgui95@outlook.com>
2023-09-25 11:35:58 +0800

    clean up sparse layer tests

---
+++ test/layer/test_sparse_ffn.py
@@ -0,0 +1,17 @@
+import time
+import torch
+from torch import nn, profiler
+from naive_gpt import kernels, layers
+
+
+class RoutedFFN(nn.Module):
+    def __init__(self):
+        nn.Module.__init__(self)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()

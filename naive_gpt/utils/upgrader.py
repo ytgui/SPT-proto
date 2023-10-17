2ac4804a6c38797bb27c1b9184c7403990d70a50
ytgui <ytgui95@outlook.com>
2023-10-17 10:50:46 +0800

    fix upgrader on routed_ffn

--- naive_gpt/utils/upgrader.py
+++ naive_gpt/utils/upgrader.py
@@ -157,10 +157,18 @@ class SparseLoRAHandler(LoRAHandler):
             return
         assert isinstance(child, layers.Feedforward)
         new_model = layers.LoRARoutedFFN.from_pretrained(
-            d_lora=self.lora_r, block_size=1024,
+            d_lora=self.lora_r, block_size=child.d_model // 2,
             p_dropout=self.lora_dropout, source=child
         )
         return new_model
+
+    def onFeedforward(self,
+                      name: str,
+                      child: layers.LLaMaFeedforward):
+        if self.stage != 2:
+            print('[SKIP]', name, type(child).__name__)
+            return
+        raise NotImplementedError
 
 
 class ModuleUpgrader:
